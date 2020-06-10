c13594e09308b51ac2a7dc4e0add100f927e2b7f
keltonhimself <zhuoran2@andrew.cmu.edu>
2020-06-10 17:29:34 -0400

    kelton

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'alex' });
+  res.render('index', { title: 'kelton' });
 });
 
 module.exports = router;
