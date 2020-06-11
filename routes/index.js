fa9400e7bf4aa69fbf898945da0e77919cf45a3a
keltonhimself <zhuoran2@andrew.cmu.edu>
2020-06-11 16:30:28 -0400

    pug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const path = require("path");
+const app = express();
+app.set("view engine","pug");
+app.set("views", path.join(__dirname, "views"));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.sendFile('home/site/wwwroot/views/index.html');
