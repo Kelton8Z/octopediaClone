e12f62583ab689713aa2eb8811d840288abe56a8
keltonhimself <zhuoran2@andrew.cmu.edu>
2020-06-11 15:10:07 -0400

    alex is napping

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'alex' });
+    res.sendFile('../views/index.html');
+    //  res.render('index', { title: 'alex' });
 });
 
 module.exports = router;
