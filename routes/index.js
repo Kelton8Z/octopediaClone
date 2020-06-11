0f14fecd4143f52e0dad74fd2bc01e4d9b1b92b7
Xiaoran0 <44989135+Xiaoran0@users.noreply.github.com>
2020-06-10 21:10:31 -0400

    Alex can do it but kelton is dumb

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('../views/index.html');
+  res.sendFile('/home/site/wwwroot/views/index.html');
   //res.render('index', { title: 'alex' });
 });
 
