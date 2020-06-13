0bb817ab0cf2ccd85edeed10b81304254681b9b6
keltonhimself <zhuoran2@andrew.cmu.edu>
2020-06-13 14:16:03 -0400

    BLM

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var actionRouter = require('./routes/action');
+var actionRouter = require('./routes/create-action');
 var app = express();
 
 // view engine setup
