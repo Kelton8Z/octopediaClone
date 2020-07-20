c369bd8e137933b8f92000954372a63ca72c4a62
yajiewang0822 <yajiew@andrew.cmu.edu>
2020-07-19 21:16:55 -0400

    add information get; link chatbot.html and login.html; chatbot self-adjust

--- Octopedia_bootstrap/js/login.js
+++ Octopedia_bootstrap/js/login.js
@@ -18,7 +18,38 @@ function sendCode() {
     }
 }
 
-function login(){
-    //need check validation
-    location.href="chatbot.html";
+function login() {
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "chatbot.html",
+        data: $('#login_in_form').serialize(),
+        success: function (result) {
+            console.log(result);//debug
+            if (result.resultIndex === 200) {
+                alert("登陆成功");
+            }
+        },
+        error: function () {
+            alert("登陆失败");
+        }
+    })
+}
+
+function signUp() {
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "chatbot.html",
+        data: $('#sign_up_form').serialize(),
+        success: function (result) {
+            console.log(result);//debug
+            if (result.resultIndex === 200) {
+                alert("注册成功");
+            }
+        },
+        error: function () {
+            alert("注册失败");
+        }
+    })
 }
